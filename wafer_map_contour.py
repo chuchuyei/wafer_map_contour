6b22dcc7c360df4dc655ffc01cb1fce50c50097f
chuchuyei <chuchuyei@gmail.com>
2018-05-18 23:11:11 +0800

    initial

---
+++ wafer_map_contour.py
@@ -0,0 +1,101 @@
+#!/usr/bin/env python
+# -*- coding:utf-8 -*-
+import os
+import numpy as np
+import scipy.interpolate
+import matplotlib.pyplot as plt
+from matplotlib.patches import Circle
+
+
+class WaferMapContour:
+    """
+    Wafer Map Contour - A powerful wafer map tool
+    """
+    @staticmethod
+    def draw_map_contour(x: list, y: list, z: list, pic_name: str, wafer_size: int = 300, **kwargs: object) -> None:
+        """ A Wafer Map Contour tool
+        Draw and output wafer map file.
+        :param x: 座標 X
+        :param y: 座標 Y
+        :param z: 座標 值
+        :param site_num: 量測點個數
+        :param pic_name: 圖片名稱
+        :param wafer_size: Wafer 大小 (default = 300)
+        :param kwargs:  vmin: 量測最小值 (default = z 最小值);
+                        vmax: 量測最大值 (default = z 最大值);
+                        output_path: 輸出圖檔的路徑(default = 預設路徑)
+       """
+
+        # Ensure data correct (fool-proofing)
+        if not (len(x) == len(y) == len(z)):
+            raise Exception('Input data size error!')
+
+        # New layer
+        fig, ax = plt.subplots()
+
+        # find boundary coordinate & value
+        boundary_x = [0, 0, (wafer_size / 2), -(wafer_size / 2)]
+        boundary_y = [(wafer_size / 2), -(wafer_size / 2), 0, 0]
+        xc = x.copy()
+        yc = y.copy()
+        zc = z.copy()
+        for i in range(len(boundary_x)):
+            xb = boundary_x[i]
+            yb = boundary_y[i]
+            distance = (wafer_size / 2)
+            min_coordinate = list()
+            # find minimize distance point, if plurality then get mean.
+            for j in range(len(x)):
+                d = ((xb - x[j]) ** 2 + (yb - y[j]) ** 2) ** (1 / 2)
+                if d < distance:
+                    min_coordinate = []
+                    min_coordinate.append(j)
+                    distance = d
+                elif d == distance:
+                    min_coordinate.append(j)
+            min_value = [z[i] for i in min_coordinate]
+            value = np.mean(min_value)
+            xc.append(xb)
+            yc.append(yb)
+            zc.append(value)
+
+        xc = np.array(xc)
+        yc = np.array(yc)
+        zc = np.array(zc)
+
+        # Set up a regular grid of interpolation points
+        xi, yi = np.linspace(xc.min(), xc.max(), 100), np.linspace(yc.min(), yc.max(), 100)
+        xi, yi = np.meshgrid(xi, yi)
+
+        # Interpolate
+        rbf = scipy.interpolate.Rbf(xc, yc, zc, function='thin_plate')
+        zi = rbf(xi, yi)
+
+        vmin = kwargs['vmin'] if 'vmin' in kwargs else zc.min()
+        vmax = kwargs['vmax'] if 'vmax' in kwargs else zc.max()
+
+        im = ax.imshow(zi, vmin=vmin, vmax=vmax, origin='lower',
+                       extent=[xc.min(), xc.max(), yc.min(), yc.max()], cmap='gist_rainbow_r',
+                       )
+
+        circle = Circle((0, 0), (wafer_size / 2), facecolor='none', linewidth=1)
+        ax.add_patch(circle)
+        im.set_clip_path(circle)
+        fig.colorbar(im)
+
+        xj = np.array(x)
+        yj = np.array(y)
+        zj = np.array(z)
+        plt.plot(xj, yj, 'o', color='black', ms=5)
+
+        for j in range(len(xj)):
+            plt.text(xj[j], yj[j] + 7, str("{0:.2f}".format(round(zj[j], 2))), horizontalalignment='center',
+                     verticalalignment='bottom')
+
+        # Output picture
+        if 'output_path' in kwargs:
+            plt.savefig(os.path.join(kwargs['output_path'], pic_name))
+        else:
+            plt.savefig(os.path.join(pic_name))
+        plt.close(fig)
+
